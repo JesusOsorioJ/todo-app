a62763112abea681eaf33a69b36a6f5e9b1b0c92
Jesus Osorio <josorio@disruptivestudio.com>
2024-12-04 11:14:05 -0500

    Añadir modelos

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { TaskListComponent } from '../pages/tasks/task-list/task-list.component';
+import { TaskFormComponent } from '../pages/tasks/task-form/task-form.component';
+import { CategoryListComponent } from '../pages/category/category-list/category-list.component';
+import { CategoryFormComponent } from '../pages/category/task-form/category-form.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+  },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/new', component: TaskFormComponent },
+  { path: 'tasks/:id', component: TaskFormComponent },
+  { path: 'categories', component: CategoryListComponent },
+  { path: 'categories/new', component: CategoryFormComponent },
+  { path: 'categories/:id', component: CategoryFormComponent },
 ];
 
 @NgModule({
